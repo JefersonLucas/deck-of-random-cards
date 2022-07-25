b8727f5d0cb7059561742bd311a5bf434e4bdf91
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-25 17:40:29 -0300

    Created menu component

---
+++ src/components/Menu/styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Container = styled.head`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  text-align: center;
+
+  padding: 0 20px;
+  background-color: ${({ theme }) => theme.colors.background.tertiary};
+  color: ${({ theme }) => theme.colors.text.white};
+`;
+
+export const UserContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px;
+
+  svg {
+    width: 35px;
+    height: 35px;
+    color: ${({ theme }) => theme.colors.others.orange};
+  }
+`;
+
+export const Username = styled.strong``;
