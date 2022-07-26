a6f4d3a49fed9e96bfa575dadba627d70e3c9730
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-26 10:44:14 -0300

    Created styles

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,3 +1,24 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: grid;
+
+  grid-template-columns: auto;
+  grid-template-rows: 80px auto;
+`;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const CardsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 15px;
+  padding: 20px;
+`;
