d0dad7d9482d84b9898020b08a0ea31916178674
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-26 11:57:13 -0300

    Added array cards

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,7 +1,66 @@
-import { Container } from "./styles";
+import Button from "../Button";
+import { MdShuffle, MdFlipCameraAndroid } from "react-icons/md";
+
+import { Container, ButtonsContainer, CardsContainer } from "./styles";
+import Card from "../Card";
+import { ICard } from "../Card/types";
 
 const Content: React.FC = () => {
-  return <Container>Content</Container>;
+  const cards: ICard[] = [
+    {
+      src: "https://www.gstatic.com/webp/gallery/1.jpg",
+      name: "montain",
+      points: Math.floor(Math.random() * 10),
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ex    iure deleniti distinctio explicabo doloribus reiciendis qui laborum debitis inventore.",
+    },
+    {
+      src: "https://www.gstatic.com/webp/gallery/1.jpg",
+      name: "montain",
+      points: Math.floor(Math.random() * 10),
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ex    iure deleniti distinctio explicabo doloribus reiciendis qui laborum debitis inventore.",
+    },
+    {
+      src: "https://www.gstatic.com/webp/gallery/1.jpg",
+      name: "montain",
+      points: Math.floor(Math.random() * 10),
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ex    iure deleniti distinctio explicabo doloribus reiciendis qui laborum debitis inventore.",
+    },
+    {
+      src: "https://www.gstatic.com/webp/gallery/1.jpg",
+      name: "montain",
+      points: Math.floor(Math.random() * 10),
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ex    iure deleniti distinctio explicabo doloribus reiciendis qui laborum debitis inventore.",
+    },
+    {
+      src: "https://www.gstatic.com/webp/gallery/1.jpg",
+      name: "montain",
+      points: Math.floor(Math.random() * 10),
+      description:
+        "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid ex    iure deleniti distinctio explicabo doloribus reiciendis qui laborum debitis inventore.",
+    },
+  ];
+  return (
+    <Container>
+      <ButtonsContainer>
+        <Button variant="cyan">
+          Shuffle <MdShuffle />
+        </Button>
+        <Button variant="orange">
+          Pull
+          <MdFlipCameraAndroid />
+        </Button>
+      </ButtonsContainer>
+      <CardsContainer>
+        {cards.map((item) => (
+          <Card {...item} key={Math.random() * item.points} />
+        ))}
+      </CardsContainer>
+    </Container>
+  );
 };
 
 export default Content;
