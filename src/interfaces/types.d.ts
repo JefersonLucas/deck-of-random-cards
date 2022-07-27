85d236b58f1f39c4f01c1f8458d4a0c17693d922
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-27 14:25:38 -0300

    Removed properties from the IData interface

--- src/interfaces/types.d.ts
+++ src/interfaces/types.d.ts
@@ -1,16 +1,6 @@
 export interface IData {
-  name: string;
-  latin_name: string;
-  animal_type: string;
-  active_time: string;
-  length_min: string;
-  length_max: string;
-  weight_min: string;
-  weight_max: string;
-  lifespan: string;
-  habitat: string;
-  diet: string;
-  geo_range: string;
-  image_link: string;
   id: number;
+  name: string;
+  diet: string;
+  image_link: string;
 }
