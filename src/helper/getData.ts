f9fc6da96ee215def10b7445cf1d22a5d02594a7
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-27 14:17:14 -0300

    Refactored helper

--- src/helper/getData.ts
+++ src/helper/getData.ts
@@ -1,15 +1,16 @@
 import { IData } from "../interfaces/types";
 
 async function getData(number: number): Promise<IData[]> {
+  // https://zoo-animal-api.herokuapp.com/
   const API = `https://zoo-animal-api.herokuapp.com/animals/rand/${number}`;
+
   const request = await fetch(API);
+
   const response = await request.json();
 
-  if (!request.ok) {
-    throw new Error(response.error);
-  }
+  if (!request.ok) throw new Error(response.error);
 
-  return response as IData[];
+  return response;
 }
 
 export default getData;
