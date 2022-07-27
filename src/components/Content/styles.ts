2cf43ab9a07a5921f1e35ffe82523e85ceddca8a
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-27 14:27:49 -0300

    Added style for suffle elements

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Suffle } from "./types";
 
 export const Container = styled.div`
   display: grid;
@@ -7,6 +8,10 @@ export const Container = styled.div`
   grid-template-rows: 80px auto;
   align-items: center;
   justify-content: center;
+
+  p {
+    text-align: center;
+  }
 `;
 
 export const ButtonsContainer = styled.div`
@@ -16,11 +21,28 @@ export const ButtonsContainer = styled.div`
   gap: 10px;
 `;
 
-export const CardsContainer = styled.div`
+export const CardsContainer = styled.div<Suffle>`
   display: flex;
   align-items: center;
   justify-content: center;
+  flex-direction: row;
   flex-wrap: wrap;
-  gap: 15px;
+  gap: 10px;
   padding: 20px;
+
+  div:nth-child(1) {
+    order: ${({ suffle }) => suffle[0]};
+  }
+  div:nth-child(2) {
+    order: ${({ suffle }) => suffle[1]};
+  }
+  div:nth-child(3) {
+    order: ${({ suffle }) => suffle[2]};
+  }
+  div:nth-child(4) {
+    order: ${({ suffle }) => suffle[3]};
+  }
+  div:nth-child(5) {
+    order: ${({ suffle }) => suffle[4]};
+  }
 `;
