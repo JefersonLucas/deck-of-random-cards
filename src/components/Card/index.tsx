816b18bac08ed053ee7cba959d25c584592c4ba2
Jeferson Lucas <jeferson.luckas@gmail.com>
2022-07-27 14:15:52 -0300

    Added active class

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,13 +2,13 @@ import { RiQuestionMark } from "react-icons/ri";
 import { Container } from "./styles";
 import { ICard } from "./types";
 
-const Card: React.FC<ICard> = ({ src, name, points, description }) => {
+const Card: React.FC<ICard> = ({ src, name, points, description, active }) => {
   return (
     <Container>
-      <div className="front">
+      <div className={`front ${active && "active"}`}>
         <RiQuestionMark />
       </div>
-      <div className="back">
+      <div className={`back ${active && "active"}`}>
         <img src={src} draggable={false} alt={name} />
         <h3>{name}</h3>
         <strong>{points}</strong>
